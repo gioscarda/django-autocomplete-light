08c4eadb7ac6b32f1183b1fb09123e7faab045c5
gioscarda <gioscarda@gmail.com>
2023-12-22 15:43:15 +0100

    Fix import in __init__

--- autocomplete_light/__init__.py
+++ autocomplete_light/__init__.py
@@ -1,11 +1,9 @@
 """
 Provide tools to enable nice autocompletes in your Django project.
 """
-import django
 
-if django.VERSION < (1, 9):
-    from .shortcuts import *  # noqa
+from .shortcuts import *  # noqa
 
 default_app_config = 'autocomplete_light.apps.AutocompleteLightConfig'
 
-__version__ = (2, 3, 4)
+__version__ = (3, 0, 0)
